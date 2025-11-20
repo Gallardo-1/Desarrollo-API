56351435ddc2e81dfd0c188d49a88540e4e245ea
keniadev <kenializbethcontreras@outlook.es>
2025-11-19 18:08:20 -0600

    funcionalidad de agregar putuacion y obtener promedios por producto y el mejor producto terminado

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RatingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,8 @@ Route::get('/user', function (Request $request) {
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); 
+//ENDPOINT PARA AGREGAR/ACTUALIZAR PUNTUACIONES
+Route::middleware('auth:sanctum')->post('/products/{product}/ratings', [RatingController::class, 'store']); 
+//ENDPOINT PARA OBTENER EL PRODUCTO MEJOR VALORADO 
+Route::get('/products/best-rated', [RatingController::class, 'bestRatedProduct']);
